a7a592c19bec2349d85f4eadcf2863a1550507f0
Afsinur <afsin.sayem1@gmail.com>
2023-03-12 20:47:16 +0600

    vw, rvw img ext changed

--- src/components/home/relatedViewCollection.js
+++ src/components/home/relatedViewCollection.js
@@ -83,28 +83,28 @@ const CatalogContentDivForViewCollectionRelated = styled.div`
 const RelatedViewCollection = () => {
   let imgDataArr = [
     {
-      onLoadImg: "/img/bottomProductImage/01BottomProduct_home.jpg",
+      onLoadImg: `/img/bottomProductImage/01BottomProduct_home.jpg`,
       onLoadTitle: "Classic mafia tee",
       onLoadSubTitles: "$24.98",
       hoverSubTitles: ["s", "m", "l"],
       customize: "ClassicMafiaTee",
     },
     {
-      onLoadImg: "/img/bottomProductImage/02BottomProduct_home.JPG",
+      onLoadImg: `/img/bottomProductImage/02BottomProduct_home.JPG`,
       onLoadTitle: "Classic mafia long sleave",
       onLoadSubTitles: "$48.98",
       hoverSubTitles: ["s", "m", "l"],
       customize: "ClassicMafiaLongSleave",
     },
     {
-      onLoadImg: "/img/bottomProductImage/03BottomProduct_home.JPG",
+      onLoadImg: `/img/bottomProductImage/03BottomProduct_home.JPG`,
       onLoadTitle: "Classic 24k tee",
       onLoadSubTitles: "$27.98",
       hoverSubTitles: ["s", "m", "l"],
       customize: "Classsic24ktTee",
     },
     {
-      onLoadImg: "/img/bottomProductImage/04BottomProduct_home.jpg",
+      onLoadImg: `/img/bottomProductImage/04BottomProduct_home.jpg`,
       onLoadTitle: "Classic mafia hoodie",
       onLoadSubTitles: "$77.74",
       hoverSubTitles: ["s", "m", "l"],
--- src/components/home/viewCollection.js
+++ src/components/home/viewCollection.js
@@ -78,28 +78,28 @@ const CatalogContentDivForViewCollection = styled.div`
 const ViewCollection = () => {
   let imgDataArr = [
     {
-      onLoadImg: "/img/topProductImage/01Product_home.jPG",
+      onLoadImg: `/img/topProductImage/01Product_home.jPG`,
       onLoadTitle: "Classic mafia tee",
       onLoadSubTitles: "$24.98",
       hoverSubTitles: ["s", "m", "l"],
       customize: "ClassicMafiaTee",
     },
     {
-      onLoadImg: "/img/topProductImage/02Product_home.jpg",
+      onLoadImg: `/img/topProductImage/02Product_home.jpg`,
       onLoadTitle: "Classic mafia long sleave",
       onLoadSubTitles: "$48.98",
       hoverSubTitles: ["s", "m", "l"],
       customize: "ClassicMafiaLongSleave",
     },
     {
-      onLoadImg: "/img/topProductImage/03Product_home.JPG",
+      onLoadImg: `/img/topProductImage/03Product_home.JPG`,
       onLoadTitle: "Classic 24k tee",
       onLoadSubTitles: "$27.98",
       hoverSubTitles: ["s", "m", "l"],
       customize: "Classsic24ktTee",
     },
     {
-      onLoadImg: "/img/topProductImage/04Product_home.JPG",
+      onLoadImg: `/img/topProductImage/04Product_home.JPG`,
       onLoadTitle: "Classic 24k hoodie signature edition",
       onLoadSubTitles: "$124.24",
       hoverSubTitles: ["s", "m", "l"],
